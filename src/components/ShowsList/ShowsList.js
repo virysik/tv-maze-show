5fa54a7efba370b5e13df70f457ab85c0c7ab18a
virysik <68701496+virysik@users.noreply.github.com>
2021-10-23 14:07:50 +0200

    added show list with styles file

--- src/components/ShowsList/ShowsList.js
+++ src/components/ShowsList/ShowsList.js
@@ -1,9 +1,21 @@
 import ShowsListItem from '../ShowsListItem'
+import { Ul } from './ShowsList.styled'
 
-export default function ShowsList() {
+export default function ShowsList({ searchItems }) {
   return (
-    <ul>
-      <ShowsListItem />
-    </ul>
+    <Ul>
+      {searchItems.map(({ id, img, title, description, rating, url }) => {
+        return (
+          <ShowsListItem
+            key={id}
+            img={img}
+            title={title}
+            description={description}
+            rating={rating}
+            url={url}
+          />
+        )
+      })}
+    </Ul>
   )
 }
