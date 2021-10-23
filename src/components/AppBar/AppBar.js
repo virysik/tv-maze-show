6432ffa12f91580ddca1ddfd1785ddec098ca777
virysik <68701496+virysik@users.noreply.github.com>
2021-10-23 14:09:03 +0200

    added functionality to app bar and styles

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,19 +1,136 @@
 import Footer from '../Footer'
 import SearchForm from 'components/SearchForm'
 import ShowsList from 'components/ShowsList'
+import Spinner from 'components/Spinner'
+import toast from 'react-hot-toast'
+import { useState, useEffect } from 'react'
+import { MdOutlineReportProblem } from 'react-icons/md'
+import { fetchShows } from 'services/api'
+import { Status } from '../../constants/ApiStatus'
+import {
+  Header,
+  Container,
+  Title,
+  Main,
+  MainTitle,
+  Text,
+  Wrapper,
+} from './AppBar.styled'
 
 export default function AppBar() {
-  return (
-    <>
-      <header>
-        <h2>The most popular tv-shows</h2>
-        <SearchForm />
-      </header>
-      <main>
-        <h1>TV-shows</h1>
-        <ShowsList />
-      </main>
-      <Footer />
-    </>
-  )
+  const [query, setQuery] = useState('')
+  const [shows, setShows] = useState(null)
+  const [reqStatus, setReqStatus] = useState(Status.IDLE)
+
+  useEffect(() => {
+    async function fetchApi() {
+      setReqStatus(Status.PENDING)
+      try {
+        const data = await fetchShows(query)
+
+        if (!data.length) {
+          return toast('Please enter the correct search query name', {
+            icon: <MdOutlineReportProblem />,
+          })
+        }
+
+        setShows(data)
+        setReqStatus(Status.RESOLVED)
+      } catch (error) {
+        setReqStatus(Status.REJECTED)
+        console.log(error)
+      }
+    }
+
+    if (query !== '') {
+      fetchApi()
+    }
+  }, [query])
+
+  const handleSubmit = (item) => {
+    setQuery(item)
+  }
+
+  if (reqStatus === Status.IDLE) {
+    return (
+      <Wrapper>
+        <Header>
+          <Container>
+            <Title>Most popular TV shows</Title>
+            <SearchForm onFormSubmit={handleSubmit} />
+          </Container>
+        </Header>
+        <Main>
+          <Container>
+            <MainTitle>TV-shows</MainTitle>
+            <Text>Your selected tv programs will appear here...</Text>
+          </Container>
+        </Main>
+        <Footer />
+      </Wrapper>
+    )
+  }
+
+  if (reqStatus === Status.PENDING) {
+    return (
+      <Wrapper>
+        <Header>
+          <Container>
+            <Title>Most popular TV shows</Title>
+            <SearchForm onFormSubmit={handleSubmit} />{' '}
+          </Container>
+        </Header>
+        <Main>
+          <Container>
+            <MainTitle>TV-shows</MainTitle>
+            <Spinner />
+          </Container>
+        </Main>
+        <Footer />
+      </Wrapper>
+    )
+  }
+
+  if (reqStatus === Status.RESOLVED) {
+    return (
+      <Wrapper>
+        <Header>
+          <Container>
+            <Title>Most popular TV shows</Title>
+            <SearchForm onFormSubmit={handleSubmit} />{' '}
+          </Container>
+        </Header>
+        <Main>
+          <Container>
+            <MainTitle>TV-shows</MainTitle>
+            {shows && <ShowsList searchItems={shows} />}
+          </Container>
+        </Main>
+        <Footer />
+      </Wrapper>
+    )
+  }
+
+  if (reqStatus === Status.REJECTED) {
+    return (
+      <Wrapper>
+        <Header>
+          <Container>
+            <Title>Most popular TV shows</Title>
+            <SearchForm onFormSubmit={handleSubmit} />{' '}
+          </Container>
+        </Header>
+        <Main>
+          <Container>
+            <MainTitle>TV-shows</MainTitle>
+            <Text>
+              <MdOutlineReportProblem />
+              Something went wrong. Please try again later.
+            </Text>
+          </Container>
+        </Main>
+        <Footer />
+      </Wrapper>
+    )
+  }
 }
--- src/components/AppBar/AppBar.styled.js
+++ src/components/AppBar/AppBar.styled.js
@@ -1,1 +1,77 @@
 import styled from '@emotion/styled'
+import defaultImg from 'images/img8.jpg'
+import defaultImgOne from 'images/img4.jpg'
+export const Container = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 30px;
+  padding-right: 30px;
+  max-width: 1024px;
+`
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-height: 100vh;
+  background-image: url(${defaultImgOne});
+  background-position: 50% 50%;
+  background-repeat: no-repeat;
+  background-size: cover;
+`
+
+export const Header = styled.header`
+  min-height: 50px;
+  padding-top: 60px;
+  padding-bottom: 60px;
+  text-align: center;
+  color: var(--primary-color);
+  background-image: url(${defaultImg});
+  background-position: 50% 50%;
+  background-repeat: repeat;
+  background-size: contain;
+`
+
+export const Title = styled.h2`
+  margin-bottom: 20px;
+  font-size: 48px;
+`
+
+export const Main = styled.main`
+  padding-top: 40px;
+  padding-bottom: 20px;
+`
+export const MainTitle = styled.h1`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  border: 0;
+  padding: 0;
+  white-space: nowrap;
+  clip-path: inset(100%);
+  clip: rect(0 0 0 0);
+  overflow: hidden;
+`
+
+export const Text = styled.p`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+
+  font-size: 30px;
+  font-weight: 700;
+  text-align: center;
+
+  color: var(--dark-color);
+  animation: scale 2s;
+  @keyframes scale {
+    0% {
+      transform: scale(0.1);
+    }
+
+    100% {
+      transform: scale(1);
+    }
+  }
+`
