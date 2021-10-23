73b7d3b3398e21e3bd7950ebffd97287a3b5803a
virysik <68701496+virysik@users.noreply.github.com>
2021-10-23 14:03:53 +0200

    added container for notification

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css'
 import AppBar from './components/AppBar'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
   return (
     <>
       <AppBar />
+      <Toaster />
     </>
   )
 }
