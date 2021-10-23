d2f6d8b7b8c75e340d03c9fc950327f0952a0b95
virysik <68701496+virysik@users.noreply.github.com>
2021-10-23 19:43:06 +0200

    added function to fetch seasons

--- src/services/api.js
+++ src/services/api.js
@@ -1,22 +1,33 @@
 import axios from 'axios'
 import { API } from 'constants/API'
-import defaultImg from 'images/defaultPoster.jpg'
+import defaultImg from 'images/poster.jpg'
 
 axios.defaults.baseURL = API.BASE_URL
 
 export const fetchShows = async (query) => {
   const params = `/search/shows?q=${query}`
   const { data } = await axios.get(params)
-  return data.map((item) => {
+  return data.map(({ show }) => {
     return {
-      id: item.show.id,
-      img: item.show.image?.original ? item.show.image.original : defaultImg,
-      title: item.show.name ? item.show.name : '',
-      description: item.show.summary
-        ? item.show.summary.replace(/<\/?\w>/gi, '')
-        : '',
-      rating: item.show.rating.average ? item.show.rating.average : null,
-      url: item.show.url ? item.show.url : null,
+      id: show.id,
+      img: show.image?.original ? show.image.original : defaultImg,
+      title: show.name ? show.name : '',
+      description: show.summary ? show.summary.replace(/<\/?\w>/gi, '') : '',
+      rating: show.rating.average ? show.rating.average : null,
+      language: show.language ? show.language : null,
+      genres: show.genres ? show.genres.join(', ') : null,
+    }
+  })
+}
+
+export const fetchSeasons = async (id) => {
+  const params = `/shows/${id}/seasons`
+  const { data } = await axios.get(params)
+  return data.map(({ url, image, id }) => {
+    return {
+      url: url ? url : null,
+      img: image?.original ? image.original : defaultImg,
+      id,
     }
   })
 }
