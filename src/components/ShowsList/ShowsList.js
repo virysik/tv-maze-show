440c0c419abb673df079750fd00c844eb6d4fca1
virysik <68701496+virysik@users.noreply.github.com>
2021-10-23 19:44:48 +0200

    Update ShowsList.js

--- src/components/ShowsList/ShowsList.js
+++ src/components/ShowsList/ShowsList.js
@@ -1,21 +1,30 @@
 import ShowsListItem from '../ShowsListItem'
 import { Ul } from './ShowsList.styled'
+import PropTypes from 'prop-types'
 
 export default function ShowsList({ searchItems }) {
   return (
     <Ul>
-      {searchItems.map(({ id, img, title, description, rating, url }) => {
-        return (
-          <ShowsListItem
-            key={id}
-            img={img}
-            title={title}
-            description={description}
-            rating={rating}
-            url={url}
-          />
-        )
-      })}
+      {searchItems.map(
+        ({ id, img, title, description, rating, language, genres }) => {
+          return (
+            <ShowsListItem
+              key={id}
+              id={id}
+              img={img}
+              title={title}
+              description={description}
+              rating={rating}
+              lang={language}
+              genres={genres}
+            />
+          )
+        },
+      )}
     </Ul>
   )
+}
+
+ShowsList.propTypes = {
+  searchItems: PropTypes.arrayOf(PropTypes.object).isRequired,
 }
